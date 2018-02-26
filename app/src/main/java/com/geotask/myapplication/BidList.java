9b0b9377ce2d72f9df2982373a1bbd53d800b09f
TannerShmoog <tannershmoog@gmail.com>
2018-02-26 02:30:18 -0700

    added BidList class and unit tests

---
+++ app/src/main/java/com/geotask/myapplication/BidList.java
@@ -0,0 +1,29 @@
+package com.geotask.myapplication;
+
+import java.util.ArrayList;
+
+public class BidList {
+    private ArrayList<Bid> bidArrayList = new ArrayList<Bid>();
+    private String taskID;
+
+    public BidList(String TaskID) {
+        this.taskID = TaskID;
+    }
+
+    public void deleteBid(int index) {
+        bidArrayList.remove(index);
+    }
+
+    public Bid getBid(int index) {
+        return bidArrayList.get(index);
+    }
+
+    public void addBid(Bid bid) {
+        bidArrayList.add(bid);
+    }
+
+    public int getNumBids() {
+        return bidArrayList.size();
+    }
+
+}
--- app/src/main/java/com/geotask/myapplication/TaskOld.java
+++ app/src/main/java/com/geotask/myapplication/TaskOld.java
@@ -4,6 +4,6 @@ package com.geotask.myapplication;
  * Created by felixdgaypc on 2/23/2018.
  */
 
-public class Task extends GTData {
+public class TaskOld extends GTData {
     //Template for Task class
 }
