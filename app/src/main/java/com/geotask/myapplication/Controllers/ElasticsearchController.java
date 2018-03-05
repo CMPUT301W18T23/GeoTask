1404208cc2a297baa61cb4d4cc71c81c87115478
lepoidev <35352972+lepoidev@users.noreply.github.com>
2018-03-04 19:23:17 -0700

    Added documentation

--- app/src/main/java/com/geotask/myapplication/Controllers/ElasticsearchController.java
+++ app/src/main/java/com/geotask/myapplication/Controllers/ElasticsearchController.java
@@ -6,12 +6,14 @@ import com.geotask.myapplication.DataClasses.Bid;
 import com.geotask.myapplication.DataClasses.GTData;
 import com.geotask.myapplication.DataClasses.Task;
 import com.geotask.myapplication.DataClasses.User;
+import com.geotask.myapplication.QueryBuilder.SuperBooleanBuilder;
 import com.google.gson.Gson;
 import com.searchly.jestdroid.DroidClientConfig;
 import com.searchly.jestdroid.JestClientFactory;
 import com.searchly.jestdroid.JestDroidClient;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import io.searchbox.client.JestResult;
@@ -27,10 +29,23 @@ https://github.com/searchbox-io/Jest/tree/master/jest
  */
 
 
+/**
+ * ElasticsearchController - Our heart and soul
+ *
+ */
 public class ElasticsearchController {
     private static JestDroidClient client;
     private final static String INDEX_NAME = "cmput301w18t23";
 
+    /**
+     * createNewDocument - creates the new document of whichever type it is passed
+     * Note that the getType() function is overridden by every class that extends GTData. So in
+     * other words if you pass it a Profile, it will be added to profile, if you pass it a Bid, it
+     * will be added to bid ...
+     *
+     * @param data - GTData that should be added to the database
+     * @return - ID of the document
+     */
     public String createNewDocument(GTData data){
         Gson gson = new Gson();
         String json = gson.toJson(data);
@@ -47,6 +62,13 @@ public class ElasticsearchController {
         return null;
     }
 
+    /**
+     * getDocument - Gets the
+     *
+     * @param ID - ID of the document
+     * @return GTData object
+     */
+    //TODO - What is this function supposed to do lol
     public GTData getDocument(String ID) {
         Get request = new Get.Builder(INDEX_NAME, ID).build();
 
@@ -60,6 +82,13 @@ public class ElasticsearchController {
         return null;
     }
 
+    /**
+     * deleteDocument - deletes a document with the provided id of provided type
+     *
+     * @param ID - ID of document
+     * @param type - type of document
+     * @return - response code of deletion (200 on success, 400 on failure, -1 on exception)
+     */
     public int deleteDocument(String ID, String type) {
         try {
             Log.i("build", new Delete.Builder(ID).index(INDEX_NAME).type(type).build().toString());
@@ -71,6 +100,12 @@ public class ElasticsearchController {
         return -1;
     }
 
+    /**
+     * searchBids - Method that searches the elasticSearch database and returns a list of bid objects
+     *
+     * @param query - query of terms that has been already formatted
+     * @return a list of Bid objects
+     */
     public List<Bid> searchBids(String query) {
         Search search = new Search.Builder(query).addIndex(INDEX_NAME).addType("bid").build();
         try {
@@ -84,6 +119,12 @@ public class ElasticsearchController {
         return null;
     }
 
+    /**
+     * searchTasks - Method that searches the elasticSearch database and returns a list of task objects
+     *
+     * @param query - query of terms that has been already formatted
+     * @return a list of Task objects
+     */
     public List<Task> searchTasks(String query) {
         Search search = new Search.Builder(query).addIndex(INDEX_NAME).addType("task").build();
         try {
@@ -97,6 +138,12 @@ public class ElasticsearchController {
         return null;
     }
 
+    /**
+     * searchUsers - Method that searches the elasticSearch database and returns a list of profile objects
+     *
+     * @param query - query of terms that has been already formatted
+     * @return a list of profile objects
+     */
     public List<User> searchUsers(String query) {
         Search search = new Search.Builder(query).addIndex(INDEX_NAME).addType("user").build();
         try {
@@ -110,7 +157,32 @@ public class ElasticsearchController {
         return null;
     }
 
+    /**
+     * existsProfile - Method for checking if an email is in use by another user
+     *
+     * @param email - email of the registering use
+     * @return true if the email is in use, false otherwise
+     */
+    public boolean existsProfile(String email){
+        ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add("email");
+        arrayList.add(email);
+        SuperBooleanBuilder query = new SuperBooleanBuilder();
+        query.put(arrayList);
+        Search search = new Search.Builder(query.toString()).addIndex(INDEX_NAME).addType("user").build();
+        try {
+            if(client.execute(search).getTotal() > 0){
+                return true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
+    /**
+     * verifySettings - Initializes client if not previously initialized
+     */
     public static void verifySettings() {
         if(client == null) {
             DroidClientConfig.Builder builder = new DroidClientConfig.Builder("http://cmput301.softwareprocess.es:8080");
--- app/src/main/java/com/geotask/myapplication/QueryBuilder/SuperBooleanBuilder.java
+++ app/src/main/java/com/geotask/myapplication/QueryBuilder/SuperBooleanBuilder.java
@@ -3,8 +3,11 @@ package com.geotask.myapplication.QueryBuilder;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * SuperBooleanBuilder - because every other builder breaks our dependencies
+ *
+ */
 public class SuperBooleanBuilder {
-
     private final String startString = "{\n" +
                                         "\t\"query\": {\n" +
                                         "\t\t\"bool\": {\n" +
@@ -16,7 +19,16 @@ public class SuperBooleanBuilder {
                                         "}";
     private String terms = "";
 
+    /**
+     * put (single pair) - adds a single term to the list of terms
+     *
+     * @param field - key of the pair
+     * @param term - value of the pair
+     */
     public void put(String field, String term) {
+        if (!terms.equals("")) {
+            terms += ",";
+        }
         terms +="\n{" +
                 "\t\t\t\t\"term\": {\n" +
                 "\t\t\t\t\t\"" + field + "\": \"" + term + "\"\n" +
@@ -24,6 +36,11 @@ public class SuperBooleanBuilder {
                 "\t\t\t}";
     }
 
+    /**
+     * put (multiple pairs) - adds multiple pairs to the list of terms
+     *
+     * @param parameters - list of strings that are formatted as [field1, term1, field2, term2 ...]
+     */
     public void put(ArrayList<String> parameters) {
         for(int i = 0; i < parameters.size(); i+=2) {
             if (!terms.equals("")) {
@@ -32,6 +49,12 @@ public class SuperBooleanBuilder {
             this.put(parameters.get(i), parameters.get(i+1));
         }
     }
+
+    /**
+     * toString - returns the formatted query
+     *
+     * @return - the formatted query
+     */
     public String toString() {
         return startString + terms + endString;
     }
