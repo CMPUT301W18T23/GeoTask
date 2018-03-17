4ce994f575efb1b8a546f0a33cb458fc59e70453
jejewitt <jejewitt@ualberta.ca>
2018-03-17 12:52:53 -0600

    added addBid

--- app/src/main/java/com/geotask/myapplication/TaskViewActivity.java
+++ app/src/main/java/com/geotask/myapplication/TaskViewActivity.java
@@ -1,22 +1,31 @@
 package com.geotask.myapplication;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.geotask.myapplication.Controllers.AsyncCallBackManager;
 import com.geotask.myapplication.Controllers.Helpers.AsyncArgumentWrapper;
 import com.geotask.myapplication.Controllers.MasterController;
+import com.geotask.myapplication.DataClasses.Bid;
 import com.geotask.myapplication.DataClasses.GTData;
 import com.geotask.myapplication.DataClasses.Task;
 import com.geotask.myapplication.DataClasses.User;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 
 //https://stackoverflow.com/questions/4127725/how-can-i-remove-a-button-or-make-it-invisible-in-android
@@ -31,6 +40,8 @@ public class TaskViewActivity extends AppCompatActivity  implements AsyncCallBac
     private String taskUserId;
     private Button editTaskButton;
     private Button bidButton;
+    private Button addBid;
+    private PopupWindow POPUP_WINDOW_DELETION = null;   //popup for error message
     private GTData data = null;
     private List<? extends GTData> searchResult = null;
 
@@ -50,7 +61,6 @@ public class TaskViewActivity extends AppCompatActivity  implements AsyncCallBac
         this.currentUser = (User)intent.getSerializableExtra("Id");
         this.currentuserId = currentUser.getObjectID(); //get specific ID
         currentUser = (User) getIntent().getSerializableExtra("currentUser");
-
         this.taskUserId = viewTask.getRequesterID();
         this.title = (TextView)findViewById(R.id.textViewTitle);
         this.name = (TextView)findViewById(R.id.textViewName);
@@ -58,11 +68,16 @@ public class TaskViewActivity extends AppCompatActivity  implements AsyncCallBac
         this.status = (TextView)findViewById(R.id.textViewStatus);
         this.editTaskButton = (Button) findViewById(R.id.editTaskButton);
         this.bidButton = (Button) findViewById(R.id.bidsButton);
+        this.addBid = (Button) findViewById(R.id.addBidButton);
+
+
 
 
         update();
         setupButtons();
         getTaskUser();
+
+
         if (currentuserId != taskUserId){
             View b = findViewById(R.id.editTaskButton);
 //            b.setVisibility(View.GONE);
@@ -88,6 +103,12 @@ public class TaskViewActivity extends AppCompatActivity  implements AsyncCallBac
                 intent.putExtra("task", viewTask);
                 intent.putExtra("currentUser", currentUser);
                 startActivity(intent);
+            }
+        });
+
+        this.addBid.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                triggerPopup(v);
             }
         });
 
@@ -131,6 +152,74 @@ public class TaskViewActivity extends AppCompatActivity  implements AsyncCallBac
                 finish();
             }
         }
+    }
+
+    public void triggerPopup(View view){
+
+        LayoutInflater layoutInflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.accept_bid_popout, null);
+
+        POPUP_WINDOW_DELETION = new PopupWindow(this);
+        POPUP_WINDOW_DELETION.setContentView(layout);
+        POPUP_WINDOW_DELETION.setFocusable(true);
+        POPUP_WINDOW_DELETION.setBackgroundDrawable(null);
+        POPUP_WINDOW_DELETION.showAtLocation(layout, Gravity.CENTER, 1, 1);
+
+        final EditText value = (EditText) layout.findViewById(R.id.editTextAmmount);
+
+        Button cancelBtn = (Button) layout.findViewById(R.id.btn_cancel);
+        cancelBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                POPUP_WINDOW_DELETION.dismiss();
+            }
+        });
+
+        Button acceptBtn = (Button) layout.findViewById(R.id.btn_accept_bid);
+        acceptBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                POPUP_WINDOW_DELETION.dismiss();
+//                String test = value.getText().toString();
+//                System.out.print(test);
+                Double number = Double.parseDouble(value.getText().toString());
+                addBid(number);
+                //TODO - go back to previous intent
+            }
+        });
+
+    }
+
+    private void addBid(Double value){
+    Bid bid = new Bid(currentUser.getObjectID(), value, viewTask.getObjectID());
+        MasterController.AsyncCreateNewDocument asyncCreateNewDocument =
+                new MasterController.AsyncCreateNewDocument();
+        asyncCreateNewDocument.execute(bid);
+
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//    taskBidded(); //need to uncomment when taskId is given
+    }
+
+    private void taskBidded(){  //this should hopefully work when get really data to get
+        viewTask.setStatus("Bidded");
+        MasterController.AsyncUpdateDocument asyncUpdateDocument =
+                new MasterController.AsyncUpdateDocument();
+        asyncUpdateDocument.execute(viewTask);
+
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
