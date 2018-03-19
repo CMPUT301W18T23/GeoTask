6b0242af627c66b1533be69d53be559233cf4252
FlamingLobster <mikeekimy@gmail.com>
2018-03-18 19:40:17 -0600

    added test in TestStorySeven

--- app/src/androidTest/java/com/geotask/myapplication/TestStorySevenTaskCompletionInteractions.java
+++ app/src/androidTest/java/com/geotask/myapplication/TestStorySevenTaskCompletionInteractions.java
@@ -23,9 +23,11 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.CoreMatchers.not;
 
 @RunWith(AndroidJUnit4.class)
 public class TestStorySevenTaskCompletionInteractions {
@@ -91,13 +93,7 @@ public class TestStorySevenTaskCompletionInteractions {
         onView(withId(R.id.btn_delete)).perform(click());
         pressBack();
 
-        Thread.sleep(1000);
-
         onView(withId(R.id.bidsButton)).perform(click());
-        onData(anything()).inAdapterView(withId(R.id.bidListView)).check(matches(isDisplayed()));
-
-
-        Thread.sleep(1000);
-
+        onView(withId(R.id.bidListView)).check(matches(not(hasDescendant(withText(" ")))));
     }
 }
