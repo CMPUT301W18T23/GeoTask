937e5239a016db1ee176ab49fbe7663dd189cf0b
TannerShmoog <tannershmoog@gmail.com>
2018-03-29 17:27:15 -0600

    added todo

--- app/src/main/java/com/geotask/myapplication/EditTaskActivity.java
+++ app/src/main/java/com/geotask/myapplication/EditTaskActivity.java
@@ -118,7 +118,7 @@ public class EditTaskActivity extends AbstractGeoTaskActivity implements AsyncCa
      * @see TaskViewActivity
      */
     private void deleteData() {
-
+        //TODO -- can't delete a task that is accepted or completed
         SuperBooleanBuilder builder = new SuperBooleanBuilder();
         builder.put("taskID", getCurrentTask().getObjectID());
 
