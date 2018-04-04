e7af3f6fbb60e73d627be987f4fc17a601d2082a
jejewitt <jejewitt@ualberta.ca>
2018-04-04 00:20:07 -0600

    working on TestStartFromMenuActivity

--- app/src/androidTest/java/com/geotask/myapplication/TestFilter.java
+++ app/src/androidTest/java/com/geotask/myapplication/TestFilter.java
@@ -9,6 +9,7 @@ import com.geotask.myapplication.DataClasses.User;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.Before;
 
 import static com.geotask.myapplication.AbstractGeoTaskActivity.setCurrentUser;
 
@@ -18,10 +19,14 @@ public class TestFilter {
     @Rule
     public ActivityTestRule<MenuActivity> MenuActivity = new ActivityTestRule<>(MenuActivity.class);
 
-    @Test
-    public void Filter() {
+    @Before
+    public void setUp(){
         User user = new User("kehan123456", "kehan1@gmail.com", "7801111111");
         setCurrentUser(user);
+    }
+    @Test
+    public void Filter() {
+
 
     }
 }
--- app/src/androidTest/java/com/geotask/myapplication/TestStartFromMenuActivity.java
+++ app/src/androidTest/java/com/geotask/myapplication/TestStartFromMenuActivity.java
@@ -1,13 +1,20 @@
 package com.geotask.myapplication;
 
+import android.content.Intent;
 import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.espresso.contrib.NavigationViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.Gravity;
+import android.widget.Toast;
 
+import com.geotask.myapplication.Controllers.AsyncCallBackManager;
+import com.geotask.myapplication.Controllers.Helpers.AsyncArgumentWrapper;
 import com.geotask.myapplication.Controllers.MasterController;
+import com.geotask.myapplication.DataClasses.GTData;
 import com.geotask.myapplication.DataClasses.Task;
+import com.geotask.myapplication.DataClasses.User;
+import com.geotask.myapplication.QueryBuilder.SuperBooleanBuilder;
 
 import org.junit.BeforeClass;
 import org.junit.Rule;
@@ -15,6 +22,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
@@ -22,17 +32,34 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static com.geotask.myapplication.AbstractGeoTaskActivity.setCurrentUser;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertTrue;
 
 //https://developer.android.com/training/testing/espresso/recipes.html
 @RunWith(AndroidJUnit4.class)
-public class TestStartFromMenuActivity {
-
+public class TestStartFromMenuActivity  implements AsyncCallBackManager {
+    private GTData data = null;
+    private List<? extends GTData> searchResult = null;
     @BeforeClass
     public static void oneTimeSetUp() {
         MasterController.verifySettings();
         MasterController.setTestSettings(TestServerAddress.getTestAddress());
+        User user = new User("NewUser", "User@gmail.com", "123456789");
+        MasterController.AsyncCreateNewDocument asyncCreateNewDocument
+                = new MasterController.AsyncCreateNewDocument();
+        asyncCreateNewDocument.execute(user);
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+
+        setCurrentUser(user);
+
+
         try {
             MasterController.deleteIndex();
             MasterController.createIndex();
@@ -41,6 +68,31 @@ public class TestStartFromMenuActivity {
         }
     }
 
+    private void loginCheck() {
+        loginCheck();
+        String email = "User@gmail.com";
+
+            SuperBooleanBuilder builder = new SuperBooleanBuilder();
+            builder.put("email", email);
+
+            MasterController.AsyncSearch asyncSearch =
+                    new MasterController.AsyncSearch(this);
+            asyncSearch.execute(new AsyncArgumentWrapper(builder, User.class));
+
+            List<User> result;
+            try {
+                result = (List<User>) asyncSearch.get();
+
+                /*
+                    Setting the globals
+                 */
+                setCurrentUser(result.get(0));
+
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+
+    }
     @Rule
     public ActivityTestRule<MenuActivity> menuActivityRule = new ActivityTestRule<>(MenuActivity.class);
 
@@ -61,7 +113,7 @@ public class TestStartFromMenuActivity {
                 .perform(DrawerActions.open());
 
         onView(withId(R.id.nav_view))
-                .perform(NavigationViewActions.navigateTo(R.id.nav_provider));
+                .perform(NavigationViewActions.navigateTo(R.id.nav_requester));
 
         onView(withId(R.id.fab))
                 .perform(click());
@@ -96,5 +148,16 @@ public class TestStartFromMenuActivity {
     @Test
     public void testLogInThenLogOutThenLogInThenBackButtonShouldNotGoBackToFirstLogin() {
 
+    }
+
+
+    @Override
+    public void onPostExecute(GTData data) {
+        this.data = data;
+    }
+
+    @Override
+    public void onPostExecute(List<? extends GTData> dataList) {
+        this.searchResult = dataList;
     }
 }
